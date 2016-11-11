6bbf20d6253a67813b6be15b9a3882cd078c0b24
Md. Atiqul Islam <atiqul.islam1589@gmail.com>
2016-11-11 16:55:23 +0600

    fix routing

--- app/landing/landing.route.js
+++ app/landing/landing.route.js
@@ -6,8 +6,8 @@
         .config(function ($stateProvider) {
             $stateProvider
                 .state('home.landing', {
-                    url: "/",
-                    templateUrl: "/app/landing/landing.html",
+                    url: "",
+                    templateUrl: "app/landing/landing.html",
                     controller: 'LandingController',
                     controllerAs: 'landCtrl'
                 })
