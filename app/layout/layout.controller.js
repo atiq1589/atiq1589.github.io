f638e9e1e0194974547a051b4d3f630461e0c3fb
Md. Atiqul Islam <atiqul.islam1589@gmail.com>
2016-11-12 14:15:46 +0600

    update code and angular binding

--- app/layout/layout.controller.js
+++ app/layout/layout.controller.js
@@ -7,19 +7,19 @@
     function LayoutController() {
         var vm = this;
         vm.navMenu = [{
-            id:'landing',
+            id: 'landing',
             name: 'Home',
             url: 'home.landing',
             ariaLabel: 'Home'
         }, {
-            id:'git-repo',            
+            id: 'git-repo',
             name: 'My Git Repository',
             url: 'home.git-repo',
             ariaLabel: 'Git Repository'
 
         },
         {
-            id:'resume',
+            id: 'resume',
             name: 'Resume',
             url: 'home.resume',
             ariaLabel: 'Resume'
@@ -30,8 +30,34 @@
 
         vm.User = {
             name: 'md atiqul islam',
-            avatar: '/img/avatar.jpg'
+            avatar: '/img/avatar.jpg',
+            contactInfos: [
+                {
+                    type: 'Email',
+                    info: 'maileto:atiqul.islam1589@gmail.com',
+                    icon: 'email',
+                    display: 'atiqul.islam1589@gmail.com'
+
+                }, {
+                    type: 'Phone',
+                    info: 'tel:+8801675392131',
+                    icon: 'phone',
+                    display: '+880 1675 39 2131'
+                }
+            ],
+            socialLinks: [
+                {
+                    name: 'Github',
+                    url: '//github.com/atiq1589',
+                    logo: 'github-circle'
+                },
+                {
+                    name: 'Linkedin',
+                    url: '//bd.linkedin.com/in/atiq1589',
+                    logo: 'linkedin'
+                }
+            ]
         }
-       
+
     }
 })();
